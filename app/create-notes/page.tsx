b7e2e91c55e1454a8f293ca8a3b6c403c5c39f9d
mihal277 <mihal277@gmail.com>
2024-08-11 21:44:45 +0200

    Allow saving notes in decks

--- app/create-notes/page.tsx
+++ app/create-notes/page.tsx
@@ -4,19 +4,14 @@ import { NotesCreationInputForm } from "@/components/notes-creation/input-form";
 import { NotesTable } from "@/components/notes-creation/notes-table";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useState } from "react";
-import { DECKS_STORAGE_KEY, getFromStorage } from "@/app/storage";
-import { AnkiDeck } from "@/app/anki/deck";
+import { getNotesOfGivenDeck } from "@/app/storage";
 import { AnkiNote } from "@/app/anki/note";
-import { EditableAnkiNote } from "@/components/notes-creation/editable-anki-note";
+import { AnkiNoteEditor } from "@/components/notes-creation/anki-note-editor";
 
 function CreateNotesPageContent() {
   const searchParams = useSearchParams();
-  const deckId = searchParams.get("deckId");
-  const [notes, setNotes] = useState(
-    getFromStorage(DECKS_STORAGE_KEY, []).find(
-      (deck: AnkiDeck) => deck.id === deckId,
-    )?.notes ?? [],
-  );
+  const deckId = searchParams.get("deckId")!!;
+  const [notesInDeck, setNotesInDeck] = useState(getNotesOfGivenDeck(deckId));
   const [isGenerationRunning, setIsGenerationRunning] = useState(false);
   const [generatedNote, setGeneratedNote] = useState<AnkiNote>();
 
@@ -33,15 +28,18 @@ function CreateNotesPageContent() {
               setGeneratedNote={setGeneratedNote}
             />
           ) : (
-            <EditableAnkiNote
-              isGenerationRunning={isGenerationRunning}
-              ankiNote={generatedNote as AnkiNote}
-              setGeneratedNote={setGeneratedNote}
-            />
+            !isGenerationRunning && (
+              <AnkiNoteEditor
+                ankiNote={generatedNote as AnkiNote}
+                notesInDeck={notesInDeck}
+                setNotesInDeck={setNotesInDeck}
+                setGeneratedNote={setGeneratedNote}
+              />
+            )
           )}
         </div>
         <div className="w-1/3">
-          <NotesTable ankiNotes={notes} />
+          <NotesTable ankiNotes={notesInDeck} />
         </div>
       </div>
     </>
@@ -50,7 +48,7 @@ function CreateNotesPageContent() {
 
 export default function CreateNotesPage() {
   return (
-   <Suspense>
+    <Suspense>
       <CreateNotesPageContent />
     </Suspense>
   );
